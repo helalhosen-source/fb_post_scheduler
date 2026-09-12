ebdc05a3cb3191122c737f62a78485f749fe1c6c
helalhosen-source <helalhosen@gmail.com>
2026-09-12 15:35:01 +0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,9 +39,10 @@ android {
 }
 
 kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
+    kotlinOptions {
+    jvmTarget = "17"
+}
+
 }
 
 flutter {
