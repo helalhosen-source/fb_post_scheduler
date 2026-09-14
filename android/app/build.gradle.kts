8912cdd248ab2d078db6cfeed1528ac2d0e0782a
helalhosen-source <helalhosen@gmail.com>
2026-09-14 06:28:09 +0600

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         }
     }
 compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -44,4 +45,7 @@ compileOptions {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
